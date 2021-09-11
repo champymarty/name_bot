836285f1c55f9a05a6e1845adeed17fc7134d04f
Raphael <raphaelstjean@yahoo.ca>
2021-09-11 02:34:54 -0400

    fix command line analyse the cli commands entered

--- Parser.py
+++ Parser.py
@@ -3,9 +3,11 @@ from logging import error
 
 class Parser:
 
-    def __init__(self):  
+    def __init__(self, args):  
+        self.args = args.strip().split(" ")
+        if len(self.args) > 0 and self.args[0].startswith("-"):
+            self.args.insert(0, "")
         self.parser = argparse.ArgumentParser(description='Show the history of names for a person')
-
         self.parser.add_argument("username",
             help="The username of the person you are searching for",
             type=str, nargs="+"
@@ -14,5 +16,5 @@ class Parser:
                             help="iThe maximum number of name to display")
 
 
-    def parse(self, args):
-        return self.parser.parse_known_args(args=args)
+    def parse(self):
+        return self.parser.parse_known_args(args=self.args)
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ intents.members = True
 client = commands.Bot(command_prefix="!", intents=intents)
 
 history = History()
+failedCommandTumnail = "https://media.giphy.com/media/3og0IvGtnDyPHCRaYU/giphy.gif"
 
 @client.event
 async def on_ready():
@@ -25,15 +26,38 @@ async def on_message(message):
         return
 
     if message.content.startswith('.name'):
-        parser = Parser()
-        args = message.content.split(".name")[1].strip().split(" ")
-        valid_args, unknown_args = parser.parse(args)
-        print(valid_args)
-        print(unknown_args)
+        parser = Parser(message.content.split(".name")[1].strip())
+        known_args, unkown_args = parser.parse()
+        if known_args.username == [""]:
+            await sendMessage(message, "You need to specify a user to check her/his past names", True, "No user specified error")
+            return
+        if len(unkown_args) > 0:
+            await sendMessage(message, "Invalid argument(s): {}".format(unkown_args), True, "Command error")
+            return
+        nicknames = {}
+        for member in client.get_all_members():
+            name_to_add = ""
+            if member.nick is None:
+                name_to_add = member.name
+            else:
+                name_to_add = member.nick
+            if name_to_add in nicknames:
+                nicknames[name_to_add].append(member.id)
+            else:
+                nicknames[name_to_add] = [member.id]
+        print(nicknames)
+
 
 @client.event
 async def on_member_update(before, after):
     if before.display_name != after.display_name:
         history.handle_new_name(after.guild.id, after.id, after.display_name)
+
+async def sendMessage(message, messageToDisplay, error, title):
+    embedVar = discord.Embed(title=title, description=messageToDisplay, color=0x00FF00)
+    if error:
+      embedVar.set_thumbnail(url = failedCommandTumnail)
+      embedVar.__setattr__("color", 0xFF0000)
+    await message.channel.send(embed=embedVar)
 
 client.run(os.getenv('TOKEN'))
